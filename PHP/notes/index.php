f090091f987407994d679bf66f5ddffaeca84ba3
apprenant <n.blay@it-students.fr>
2021-11-10 15:59:26 +0100

    PHP Pt1

---
+++ PHP/notes/index.php
@@ -0,0 +1,14 @@
+<?php
+//définition du tableau de notes
+$tableauNotes = array(
+  "eleve1" => (9, 12, 15, 14),
+  "eleve2" => (10, 13, 16, 14),
+  "eleve3" => (9, 18, 11, 10)
+);
+
+//affichage du tableau en brut
+print_r($tableauNotes);
+
+
+
+ ?>
